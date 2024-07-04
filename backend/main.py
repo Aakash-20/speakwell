4e6ddcc71a7d7921f943ccfcac492fdd54b4061d
Aakash-20 <skyakash480@gmail.com>
2024-07-04 23:29:05 +0530

    post api for adding user

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
 from core.config import settings
-from db.session import engine
-from db.base_class import Base
+from api.base import api_router
 
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
+def include_router(app):
+    app.include_router(api_router)
 
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, description=settings.PROJECT_DESCRIPTION, version=settings.PROJECT_VERSION)
-    create_tables()
+    include_router(app)
     return app
 
 app = start_application()
+
+
 
 @app.get("/")
 def hello():
