ee76ebba87b7445ad4d2d0b50165ccae42867328
Akash <skyakash480@gmail.com>
2024-07-09 17:58:46 +0530

    login authentication api

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+
 from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -19,6 +20,8 @@ class Settings:
 
     DATABASE_URL = str = os.getenv("DATABASE_URL")
     
-
+    SECRET_KEY = str = os.getenv("SECRET_KEY")
+    ALGORITHM = str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = int = 30
 
 settings = Settings() #object to access Settings() class
