feb87affda6e8a84ea6fbfdef6f5d741fbb00b01
Akash <skyakash480@gmail.com>
2024-07-11 00:12:31 +0530

    modified the put and delete blog where authorization will be user

--- backend/api/v1/route_blog.py
+++ backend/api/v1/route_blog.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, status, Depends, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 from db.session import get_db
+from db.models.user import User
+from api.v1.route_login import get_current_user
 from schemas.blog import CreateBlog, ShowBlog, UpdateBlog
 from db.repository.blog import create_new_blog, retrieve_blog, list_blogs, update_blog_by_id, delete_blog_by_id
 
@@ -37,9 +39,14 @@ def get_all_active_blogs(db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", response_model=ShowBlog)
-def update_blog(id: int, blog: UpdateBlog, db: Session = Depends(get_db)):
+def update_blog(id: int, blog: UpdateBlog, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
     try:
-        blog = update_blog_by_id(id=id, blog=blog,db=db, author_id=1)
+        blog = update_blog_by_id(id=id, blog=blog,db=db, author_id=current_user.id)
+        if isinstance(blog, dict):
+             raise HTTPException(
+                  detail= blog.get("error"),
+                  status_code=status.HTTP_400_BAD_REQUEST
+             )
         if not blog:
             raise HTTPException(detail=f"Blog with {id} does not exists")
         return blog
@@ -48,8 +55,8 @@ def update_blog(id: int, blog: UpdateBlog, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}")
-def delete_a_blog(id: int, db: Session = Depends(get_db)):
-     message = delete_blog_by_id(id = id, db=db, author_id=1)
+def delete_a_blog(id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+     message = delete_blog_by_id(id = id, db=db, author_id= current_user.id)
      if message.get("error"):
           raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=message.get("error"))
-     return {"message": f"Blog deleted successfully with {id}"}
+     return {"message": message.get("msg")}
