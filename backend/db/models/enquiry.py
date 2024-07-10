54d03c781356350688965977fa643b1dce8b9fbe
Akash <skyakash480@gmail.com>
2024-07-11 02:12:06 +0530

    post, get , fetch all enquiry api

---
+++ backend/db/models/enquiry.py
@@ -0,0 +1,15 @@
+from db.base_class import Base
+from sqlalchemy import Column, Integer, String, DateTime, BIGINT
+from datetime import datetime
+
+class Enquiry(Base):
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    course = Column(String, nullable=False)
+    customer_name = Column(String, nullable=False, unique=True)
+    customer_phone = Column(BIGINT, nullable=False, unique=True)
+    text = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+
+
+
---
+++ backend/db/repository/enquiry.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import desc
+from schemas.enquiry import CreateEnquiry
+from db.models.enquiry import Enquiry
+
+def create_new_enquiry(enquiry: CreateEnquiry, db: Session):
+    new_enquiry = Enquiry(
+        course=enquiry.course,
+        customer_name=enquiry.customer_name,
+        customer_phone=enquiry.customer_phone,
+        text=enquiry.text,
+    )
+    db.add(new_enquiry)
+    db.commit()
+    db.refresh(new_enquiry)
+    return new_enquiry
+
+def retrieve_enquiry(id: int, db: Session):
+    blog = db.query(Enquiry).filter(Enquiry.id == id).first()
+    return blog
+
+def list_all_enquiry(db: Session):
+    enquiry = db.query(Enquiry).order_by(desc(Enquiry.created_at)).all()
+    return enquiry
