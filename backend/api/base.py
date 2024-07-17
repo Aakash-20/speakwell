0159a14402e7de76c85b0d2c36c9385287bf6b9f
Akash <skyakash480@gmail.com>
2024-07-17 14:06:22 +0530

    routes

--- backend/api/base.py
+++ backend/api/base.py
@@ -9,6 +9,6 @@ api_router = APIRouter()
 
 api_router.include_router(route_user.router, prefix="/users", tags=["users"])
 api_router.include_router(route_blog.router, prefix="/blogs", tags=["blogs"])
-api_router.include_router(route_login.router, prefix="/auth", tags=["login"])
+api_router.include_router(route_login.router, prefix="/auth", tags=["token"])
 api_router.include_router(route_enquiry.router, prefix="/enquiry", tags=["enquiry"])
 api_router.include_router(route_contactus.router, prefix="/contactus", tags=["contact-us"])
