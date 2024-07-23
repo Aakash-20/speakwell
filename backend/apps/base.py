a6a231af3e5bf772953857fc2d5f1bffdfd4964b
Akash <skyakash480@gmail.com>
2024-07-23 15:40:24 +0530

    aboutus and gallery routes

--- backend/apps/base.py
+++ backend/apps/base.py
@@ -1,5 +1,5 @@
-from apps.v1 import route_blog, route_login,route_contactus,route_enquiry, route_index 
-from apps.v1 import route_header, route_footer, route_keyfactor, route_whyus, route_whatsapp
+from apps.v1 import route_blog, route_login,route_contactus,route_enquiry, route_index , route_gallery
+from apps.v1 import route_header, route_footer, route_keyfactor, route_whyus, route_whatsapp, route_aboutus
 from fastapi import APIRouter
 
 
@@ -15,6 +15,10 @@ app_router.include_router(route_footer.router, include_in_schema=False)
 app_router.include_router(route_keyfactor.router, include_in_schema=False)
 app_router.include_router(route_whyus.router, include_in_schema=False)
 app_router.include_router(route_whatsapp.router, include_in_schema=False)
+app_router.include_router(route_gallery.router, include_in_schema=False)
+app_router.include_router(route_aboutus.router, include_in_schema=False)
+
+
 
 
 
