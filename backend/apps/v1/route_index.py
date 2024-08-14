56affdfd39dcdb92598e9744fd49c3637a564d0c
Akash <skyakash480@gmail.com>
2024-08-14 19:29:05 +0530

    refactoring for render

--- backend/apps/v1/route_index.py
+++ backend/apps/v1/route_index.py
@@ -41,52 +41,15 @@ async def read_blogs(request: Request, db: Session = Depends(get_db)):
         )
 
 
-@router.get("/aboutUs", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("aboutUs.html", {"request": request, "message": "success"})
-
-
-@router.get("/footer")
-async def get_footer(request: Request):
-    return templates.TemplateResponse("footer.html", {"request": request})
-
-
-@router.get("/keyfactor")
-async def get_keyfactor(request: Request):
-    return templates.TemplateResponse("keyfactor.html", {"request": request})
-
-
-@router.get("/header")
-async def get_header(request: Request):
-    return templates.TemplateResponse("header.html", {"request": request})
-
-
 @router.get("/whatsapp", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("whatsapp.html", {"request": request, "message": "success"})
-
-
-@router.get("/gallery")
-async def read_images(request: Request, db: Session = Depends(get_db)):
-    images = await get_all_images_logic(db=db, request=request)
-    print(images)
-    return templates.TemplateResponse("gallery.html", {"request": request, "images": images})
-
-
-@router.get("/why_us")
-async def get_why_us(request: Request):
-    return templates.TemplateResponse("whyUs.html", {"request": request})
 
 
 @router.get("/best-spoken-english-classes-in-Nagpur", response_class=HTMLResponse)
 async def get_contact_form(request: Request, db: Session = Depends(get_db)):
     addresses = list_addresses(db=db)
     return templates.TemplateResponse("contactUs.html", {"request": request, "addresses": addresses})
-
-
-@router.get("/enquiry", response_class=HTMLResponse)
-async def get_enquiry_form(request: Request):
-    return templates.TemplateResponse("keyfactor.html", {"request": request})
 
 
 @router.get("/logout")
