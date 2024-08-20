c0631b3df21f34648a62124ef1319f79c5b93674
Akash <skyakash480@gmail.com>
2024-08-20 16:29:30 +0530

    apis of gallery in home and admin are working fine

--- backend/apps/v1/route_image.py
+++ backend/apps/v1/route_image.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter, Request, Depends, File, UploadFile, HTTPException, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
 from db.session import get_db
 from sqlalchemy.exc import IntegrityError
 from db.repository.admin import is_admin
 from db.models.image import Image
+from sqlalchemy import select
 from fastapi.security.utils import get_authorization_scheme_param
 from api.v1.route_login import get_current_user
 import os
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 
 router = APIRouter()
@@ -19,7 +21,7 @@ IMAGEDIR = "template/gallery"
 
 
 @router.post("/upload", response_class=HTMLResponse)
-async def create_upload_file(request: Request, file: UploadFile = File(...), db: Session = Depends(get_db)):
+async def create_upload_file(request: Request, file: UploadFile = File(...), db: AsyncSession = Depends(get_db)):
     token = request.cookies.get("access_token")
     _, token = get_authorization_scheme_param(token)
     try:
@@ -43,18 +45,18 @@ async def create_upload_file(request: Request, file: UploadFile = File(...), db:
         # Save image information to database (assuming you have an Image model)
         new_image = Image(filename=file.filename, url=image_url)
         db.add(new_image)
-        db.commit()
+        await db.commit()
         
         return RedirectResponse(url="/admin_image?message=Image uploaded successfully", status_code=status.HTTP_303_SEE_OTHER)
     except IntegrityError:
-        db.rollback()  
+        await db.rollback()  
         return RedirectResponse(url="/admin_image?message=An image with this name already exists", status_code=status.HTTP_303_SEE_OTHER)
     except Exception as e:
         return RedirectResponse(url="/admin_image?message=An error occurred while uploading your file", status_code=status.HTTP_303_SEE_OTHER)
 
 
 @router.get("/admin_image/delete/{image_id}")
-async def delete_image(request: Request, image_id: int, db: Session = Depends(get_db)):
+async def delete_image(request: Request, image_id: int, db: AsyncSession = Depends(get_db)):
     token = request.cookies.get("access_token")
     _, token = get_authorization_scheme_param(token)
     try:
@@ -64,7 +66,9 @@ async def delete_image(request: Request, image_id: int, db: Session = Depends(ge
                 url="/admin_index?message=You do not have permission to delete image",
                 status_code=status.HTTP_303_SEE_OTHER)
         
-        image = db.query(Image).filter(Image.id == image_id).first()
+        result = await db.execute(select(Image).filter(Image.id == image_id))
+        image = result.scalar_one_or_none()
+
         if not image:
             raise HTTPException(status_code=404, detail="Image not found")
         
@@ -72,21 +76,12 @@ async def delete_image(request: Request, image_id: int, db: Session = Depends(ge
         if os.path.exists(file_path):
             os.remove(file_path)
         
-        db.delete(image)
-        db.commit()
+        await db.delete(image)
+        await db.commit()
         
         alert = "Image deleted successfully"
         return RedirectResponse(
             f"/admin_image?alert={alert}", status_code=status.HTTP_303_SEE_OTHER)
-    except HTTPException as he:
-        return templates.TemplateResponse(
-            "error.html",
-            {"request": request, "alert": he.detail, "image_id": image_id}
-        )
+    
     except Exception as e:
-        print(f"Exception raised while deleting image: {e}")
-        db.rollback()
-        return templates.TemplateResponse(
-            "error.html",
-            {"request": request, "alert": "An error occurred", "image_id": image_id}
-        )
+        print(repr(e))
